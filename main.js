964ded96fe56a4f7d30c0b56ba92ea4455e446df
unknown <filip.gora@outlook.com>
2021-03-18 13:47:17 +0100

    nothing

--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".order").addEventListener("click", () => {
-    const form = document.querySelector(".form-box");
-    form.style.visibility = "visible";
-});
-document.querySelector(".close").addEventListener("click", () => {
-    const form = document.querySelector(".form-box");
-    form.style.visibility = "hidden";
-});
